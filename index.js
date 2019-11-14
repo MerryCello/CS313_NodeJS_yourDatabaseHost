fe55abe27e8a6794195586a375a05df01774f133
grumpyCello <kfoniciello@gmail.com>
2019-11-14 15:21:33 -0700

    initial commit. Rock paper scissors game

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+var gameEngine = require('./gameEngine');
+var app = express();
+
+app.set('port', 5000)
+   .use(express.static(__dirname + '/public'))
+   .set('views', __dirname + '/views')
+   .set('view engine', 'ejs')
+   .get('/', (req, res) => {
+       res.sendFile('form.html', { root: __dirname + "/public" });
+   })
+   .get('/game', gameEngine.playGame)
+   .listen(app.get('port'), function() {
+       console.log('Listening on port: ' + app.get('port'));
+   })
