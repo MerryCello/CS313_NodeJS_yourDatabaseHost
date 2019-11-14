1cdfba8c4850e09844d295f4f8d23ad70fea3546
grumpyCello <kfoniciello@gmail.com>
2019-11-14 15:25:58 -0700

    fixing errors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 var gameEngine = require('./gameEngine');
 var app = express();
 
-app.set('port', 5000)
+app.set('port', process.env.PORT|| 5000)
    .use(express.static(__dirname + '/public'))
    .set('views', __dirname + '/views')
    .set('view engine', 'ejs')
