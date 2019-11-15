d745910b736156e2ac6c02a475341a5ddff5882c
grumpyCello <kfoniciello@gmail.com>
2019-11-14 18:20:59 -0700

    postal rate calculator V.1.0

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-var gameEngine = require('./gameEngine');
+const calcRates = require('./calcRates');
 var app = express();
 
 app.set('port', process.env.PORT|| 5000)
@@ -9,7 +9,7 @@ app.set('port', process.env.PORT|| 5000)
    .get('/', (req, res) => {
        res.sendFile('form.html', { root: __dirname + "/public" });
    })
-   .get('/game', gameEngine.playGame)
+   .get('/results', calcRates.calcRates)
    .listen(app.get('port'), function() {
        console.log('Listening on port: ' + app.get('port'));
    })
